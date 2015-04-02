2ed6d7fa553c9e8732e6e60c6d850da78d68deda
Sayeed <sayeed@diit.info>
2015-04-02 14:22:52 +0600

    first Commit

---
+++ src/TestGit.java
@@ -0,0 +1,14 @@
+
+public class TestGit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("testing GIT");
+		
+
+	}
+
+}
